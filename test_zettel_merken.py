d59bc45d15e3bf1202ae4ae13e971aaca17230ee
Pritesh Tupe <mail@priteshtupe.com>
2022-07-15 23:11:47 +0530

    Change table schema; Update config

--- test_zettel_merken.py
+++ test_zettel_merken.py
@@ -1,7 +1,6 @@
-# USAGE: zettel-merken $ python -m pytest [ -q test/ ]
+# USAGE: zettel-merken $ python -m pytest
 
-import json
-import sqlite3 as db
+import json, sqlite3, pytest
 from pathlib import Path
 from datetime import date, timedelta
 
@@ -9,50 +8,71 @@ import zettel_merken as zm
 from config_example import *
 
 
+note1 = Path("./sample_folder/note_folder_1/note-1.md").absolute()
+note2 = Path("./sample_folder/note_folder_1/note-2.txt").absolute()
+note3 = Path("./sample_folder/note_folder_2/note-3.org").absolute()
+note4 = Path("./sample_folder/note_folder_2/note-4.norg").absolute()
+notes = [note1, note2, note3, note4]
+
+
 def test_create_schema():
     zm.create_schema()
-    with db.connect(DB_PATH) as cx:
-        cx.cursor().execute("select * from note_schedule")
+    with sqlite3.connect(DB_PATH) as cx:
+        cx.cursor().execute("SELECT * FROM note_schedule")
 
 
 def test_notes_list():
-    notes = zm.notes_list(NOTE_DIRS)
-    assert len(list(notes)) == 4
+    assert len(list(zm.notes_list(NOTE_DIRS))) == len(notes)
 
 
 def test_create_note_schedule():
-    note = Path("./sample_folder/note_folder_1/note-1.md").absolute()
-    zm.create_note_schedule(note, SCHEDULE_DAYS)
-
-    note2 = Path("./sample_folder/note_folder_1/note-2.md").absolute()
+    zm.create_note_schedule(note1, SCHEDULE_DAYS)
     zm.create_note_schedule(note2, (0,))
 
-    with db.connect(DB_PATH) as cx:
+    with sqlite3.connect(DB_PATH) as cx:
         cu = cx.cursor()
 
         schedule1 = cu.execute(
-            f"select schedule from note_schedule where note = '{note}'"
+            f"SELECT schedule FROM note_schedule WHERE note = '{note1}'"
         ).fetchone()
 
         assert len(json.loads(schedule1[0])) == len(SCHEDULE_DAYS)
 
         schedule2 = cu.execute(
-            f"select schedule from note_schedule where note = '{note2}'"
+            f"SELECT schedule FROM note_schedule WHERE note = '{note2}'"
         ).fetchone()
 
-        assert str(date.toordinal(date.today())) in json.loads(schedule2[0])
+        assert date.toordinal(date.today()) in json.loads(schedule2[0])
 
 
 def test_is_note_scheduled():
-    # TODO Also test for ScheduleNotFound and ScheduleExhausted
-    note = Path("./sample_folder/note_folder_1/note-1.md").absolute()
-    assert zm.is_note_scheduled(note) == False
-
-    note2 = Path("./sample_folder/note_folder_1/note-2.md").absolute()
+    assert zm.is_note_scheduled(note1) == False
     assert zm.is_note_scheduled(note2) == True
+    with pytest.raises(zm.ScheduleNotFound):
+        zm.is_note_scheduled(note3)
+
+
+def test_build_mail():
+    mail_content = zm.build_mail_content(notes)
+    expected_content = "\n\n".join(open(note).read() for note in notes)
+    assert mail_content == expected_content
+
+
+# Uncomment to run,
+# Must manually check if the mail was received
+# def test_send_mail():
+#     mail_content = zm.build_mail_content(notes)
+#     zm.send_mail(mail_content)
+
+
+def test_update_schedule():
+    zm.update_schedule([note2])
+    with pytest.raises(zm.ScheduleExhausted):
+        zm.is_note_scheduled(note2)
 
 
 def test_cleanup():
-    with db.connect(DB_PATH) as cx:
-        cx.cursor().execute("delete from note_schedule")
-        pass
+    with sqlite3.connect(DB_PATH) as cx:
+        cu = cx.cursor()
+        cu.execute("drop table if exists note_schedule")
+        cu.execute("drop index if exists idx_note")
--- zettel_merken.py
+++ zettel_merken.py
@@ -1,30 +1,47 @@
+#!/usr/bin/env python
+
 import os, json, smtplib, ssl, sqlite3
 
+from email.message import EmailMessage
 from pathlib import Path
+from typing import Sequence
 from datetime import date, timedelta
-from config import *
+
+# For testing purpose only
+if __name__ != "__main__":
+    import config_example as config
 
 
 def create_schema():
     """Create database schema for zettel-merken note schedules"""
-    with sqlite3.connect(DB_PATH) as cx:
+    with sqlite3.connect(config.DB_PATH) as cx:
         cu = cx.cursor()
+
         cu.execute(
             """CREATE TABLE IF NOT EXISTS note_schedule (
                 id INTEGER PRIMARY KEY ASC,
                 note TEXT NOT NULL UNIQUE,
                 stats JSON NOT NULL,
-                schedule JSON NOT NULL
+                schedule JSON,
+                sent JSON
             )"""
         )
 
+        cu.execute("CREATE UNIQUE INDEX IF NOT EXISTS idx_note ON note_schedule (note)")
 
-def notes_list(dirs):
+
+def notes_list(dirs: Sequence[str]):
     """Get a list of notes from a list of directories"""
+
     for dir in dirs:
         for note in os.listdir(dir):
-            # if Path(note).is_file() and note.endswith(("md", "txt", "org")):
-            yield Path(note).absolute()
+            if Path(note).suffix in [
+                ".md",
+                ".txt",
+                ".org",
+                ".norg",
+            ]:
+                yield Path(dir + os.sep + note).resolve()
 
 
 class ScheduleNotFound(Exception):
@@ -35,10 +52,26 @@ class ScheduleExhausted(Exception):
     """When the note has beeen sent on all the scheduled days"""
 
 
+def create_note_schedule(note: Path, schedule_days: tuple):
+    """Create a schedule for a given note"""
+
+    stats = {"mtime": os.stat(note).st_mtime}
+    schedule = sorted(
+        date.toordinal(date.today() + timedelta(s)) for s in schedule_days
+    )
+    sent = "[]"
+
+    with sqlite3.connect(config.DB_PATH) as cx:
+        cx.cursor().execute(
+            "insert into note_schedule (note, stats, schedule, sent) values (?, ?, ?, ?)",
+            (str(note), json.dumps(stats), json.dumps(schedule), sent),
+        )
+
+
 def is_note_scheduled(note: Path):
     """Check if a note is scheduled for this run"""
 
-    with sqlite3.connect(DB_PATH) as cx:
+    with sqlite3.connect(config.DB_PATH) as cx:
         data = (
             cx.cursor()
             .execute(f"SELECT schedule FROM note_schedule WHERE note = '{note}'")
@@ -48,72 +81,84 @@ def is_note_scheduled(note: Path):
         if not data:
             raise ScheduleNotFound
 
-        schedule = json.loads(data[0])
-        for scheduled_day, sent_day in schedule.items():
-            scheduled_date = date.fromordinal(int(scheduled_day))
-            if scheduled_date > date.today():
-                return False
-            if not sent_day:
-                return True
-
-        else:
+        if not data[0]:
             raise ScheduleExhausted
 
+        schedule = json.loads(data[0])
+        today = date.toordinal(date.today())
+        for day in schedule:
+            if day <= today:
+                return True
 
-def create_note_schedule(note: Path, schedule_days: tuple):
-    """Create a schedule for a given note"""
-
-    os_stat = os.stat(note)
-    stats = json.dumps({"size": os_stat.st_size, "mtime": os_stat.st_mtime})
-    schedule = json.dumps(
-        {date.toordinal(date.today() + timedelta(s)): None for s in schedule_days}
-    )
-    with sqlite3.connect(DB_PATH) as cx:
-        cx.cursor().execute(
-            "insert into note_schedule (note, stats, schedule) values (?, ?, ?)",
-            (str(note), stats, schedule),
-        )
+        return False
 
 
-def build_mail(notes: Path):
+def build_mail_content(notes: list[Path]):
     """Create a mail from a list of notes"""
+    return "\n\n".join(open(note).read() for note in notes)
 
 
-def send_mail(mail_list):
+def send_mail(mail_content: str):
     """Send an email containing contents of notes list scheduled for this run"""
 
+    msg = EmailMessage()
+    msg["Subject"] = "Zettel Merken Daily Review"
+    msg["From"] = config.EMAIL["USER"]
+    msg["To"] = config.RECEIVERS
+    msg.set_content(mail_content)
+
     with smtplib.SMTP_SSL(
-        EMAIL["HOST"], EMAIL["PORT"], context=ssl.create_default_context()
+        config.EMAIL["HOST"], config.EMAIL["PORT"], context=ssl.create_default_context()
     ) as server:
-        server.login(EMAIL["USER"], EMAIL["PASS"])
-        for r in RECEIVERS:
-            server.sendmail(EMAIL["USER"], r, mail_list)
+        server.login(config.EMAIL["USER"], config.EMAIL["PASS"])
+        server.send_message(msg)
 
 
-def update_schedule(mail_list):
+def update_schedule(notes: list[Path]):
     """If a note was mailed, update in database"""
+
+    notes_list = [str(note) for note in notes]
+
+    with sqlite3.connect(config.DB_PATH) as cx:
+        cu = cx.cursor()
+
+        notes_to_update = cu.execute(
+            f"SELECT id, schedule, sent FROM note_schedule WHERE note IN ({', '.join('?' * len(notes_list))})",
+            notes_list,
+        )
+
+        today = date.toordinal(date.today())
+        for (id, schedule, sent) in notes_to_update:
+            schedule = json.loads(schedule).pop()
+            sent = json.loads(sent).append(today)
+            cu.execute(
+                "UPDATE note_schedule SET schedule = (?) and sent = (?) WHERE id = (?)",
+                (schedule, sent, id),
+            )
 
 
 def main():
     create_schema()
 
-    mail_list = []
+    scheduled_notes: list[Path] = []
 
-    for note in notes_list(NOTE_DIRS):
+    for note in notes_list(config.NOTE_DIRS):
         try:
-            # Add max notes limit here
             if is_note_scheduled(note):
-                mail_list.append(note)
+                scheduled_notes.append(note)
+                if len(scheduled_notes) > config.MAX_NOTES_PER_MAIL:
+                    break
         except ScheduleNotFound:
-            create_note_schedule(note, SCHEDULE_DAYS)
+            create_note_schedule(note, config.SCHEDULE_DAYS)
         except ScheduleExhausted:
-            # Check if end with schedule or send every last scheduled day
-            pass
+            continue
 
-    # send_mail(mail_list)
-
-    update_schedule(mail_list)
+    mail_content = build_mail_content(scheduled_notes)
+    send_mail(mail_content)
+    update_schedule(scheduled_notes)
 
 
 if __name__ == "__main__":
+    import config
+
     main()
